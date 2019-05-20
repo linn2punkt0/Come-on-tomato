863306b068de9b5cb90fdb355bdaab40d0838f9d
root <root@DESKTOP-2TKMD5S.localdomain>
2019-05-20 08:21:39 +0200

    Creation of tomato movements

--- sprites/Tomato.js
+++ sprites/Tomato.js
@@ -6,24 +6,36 @@ class Tomato extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y, texture) {
     super(scene, x, y, texture);
     this.scene = scene;
-    // scene.add.existing(this);
   }
 
   preload() {
-    // console.log(this.scene);
-    // this.scene.load.image("tomato", TomatoImg);
   }
 
   create() {
-    const tomato = this.scene.add.sprite(this.x, this.y, "tomato");
-    tomato.setScale(0.07);
-    // cursors = this.input.keyboard.createCursorKeys();
+    this.tomato = this.scene.add.sprite(this.x, this.y, "tomato");
+    this.tomato.setScale(0.07);
+
+    this.cursors = this.scene.input.keyboard.createCursorKeys();
+
   }
 
   update() {
-    // if (cursors.down.isDown()) {
-    //   tomato.y += 1;
-    // }
+    //Defining tomato movement
+    if (this.cursors.down.isDown) {
+      this.tomato.y += 3;
+    }
+
+    else if (this.cursors.up.isDown) {
+      this.tomato.y -= 3;
+    }
+
+    if (this.cursors.right.isDown) {
+      this.tomato.x += 3;
+    }
+
+    else if (this.cursors.left.isDown) {
+      this.tomato.x -= 3;
+    }
   }
 }
 
