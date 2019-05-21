911f7d7bd11af9afebb70574bb0aea11846635d7
root <root@DESKTOP-2TKMD5S.localdomain>
2019-05-21 14:53:54 +0200

    Game over comments

--- scenes/GameOver.js
+++ scenes/GameOver.js
@@ -9,18 +9,28 @@ class GameOver extends Phaser.Scene {
   }
 
   create() {
+    //Game over text
     this.gameOverText = this.add.text(400, 300, "Game Over", {
       font:"80px Impact",
       align:'center',
     });
     this.gameOverText.setOrigin(0.5);
 
+    //Play again t3ext
+    this.restartText = this.add.text(400, 600, "Press space to play again", {
+      font:"20px",
+      align:'center',
+    });
+    this.restartText.setOrigin(0.5);
+
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
+    //Changing scene to game on scpace tap
     if (this.cursors.space.isDown) {
       this.scene.start("Game");
+      console.log(this.cursors);
     }
   }
 }
