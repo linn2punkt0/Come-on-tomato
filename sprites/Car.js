838937bde943de20da9c3688264fa4c7d48d796a
root <root@DESKTOP-2TKMD5S.localdomain>
2019-05-24 10:27:00 +0200

    New car class

---
+++ sprites/Car.js
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+import Game from "../scenes/Game";
+
+class Car extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y, texture) {
+    super(scene, x, y, texture);
+    this.scene = scene;
+    // scene.add.existing(this);
+    this.car = this.scene.physics.add.sprite(this.x, this.y, "car");
+    this.car.setScale(0.15);
+
+    this.car.setCollideWorldBounds(true);
+  }
+
+  preload() {}
+
+  create() {
+  }
+
+  update() {
+  }
+}
+
+export default Car;
